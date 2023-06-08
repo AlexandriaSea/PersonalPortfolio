45ea80c822f99ee8b7717720b8815c5a14ece098
Wenjie Zhou <jack-zwj@hotmail.com>
2023-06-08 00:46:31 -0400

    Bug fix

--- server.js
+++ server.js
@@ -8,9 +8,7 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var express = require('./config/express');
 var app = express();
-var path = require('path');
 
-app.use(express.static(path.join(__dirname, 'public')));
 app.listen(3000); // Test web at localhost 3000
 module.exports = app;
 console.log('Server running at http://localhost:3000/');
